3af45504c4d57185940133bea4b9e0dc2e20e60f
Anton Chernichkin <chetoony@gmail.com>
2021-10-09 17:18:57 +0300

    Работа с массивами и объектами. Рассчет суммы длин песен. Файл 6

--- 06_songs_list.py
+++ 06_songs_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+def toFixed(numObj, digits=0):
+    return f"{numObj:.{digits}f}"
 
 # Есть список песен группы Depeche Mode со временем звучания с точносттю до долей минут
 
@@ -21,6 +23,10 @@ violator_songs_list = [
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
 
 # TODO здесь ваш код
+vsl = violator_songs_list
+summ = vsl[3][1] + vsl[5][1] + vsl[8][1]
+summ_1 = toFixed(summ, 2)
+print("Три песни звучат " + str(summ_1) + " минут")
 
 # Есть словарь песен группы Depeche Mode
 violator_songs_dict = {
@@ -39,3 +45,8 @@ violator_songs_dict = {
 #   А другие три песни звучат ХХХ минут
 
 # TODO здесь ваш код
+vsd = violator_songs_dict
+# print(vsd['Sweetest Perfection'])
+summ_2 = vsd['Sweetest Perfection'] + vsd['Policy of Truth'] + vsd['Blue Dress']
+summ_3 = toFixed(summ_2, 2)
+print("А другие три песни звучат " + str(summ_3) + " минут")
