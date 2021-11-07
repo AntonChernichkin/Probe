569fbec923c172db642771c43eb165877675c3d6
”Anton <chetoony@gmail.com>
2021-11-07 22:30:52 +0300

    Закончил баблы

--- hw3/00_bubbles.py
+++ hw3/00_bubbles.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from random import random
+import random as random
 
 import simple_draw as sd
 
@@ -16,10 +16,11 @@ center_point = sd.get_point(100, 100)
 
 
 # Написать функцию рисования пузырька, принммающую 2 (или более) параметра: точка рисовании и шаг
+colors = [sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN, sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE]
 
-def bubble(center_point_def, step, radius):
+def bubble(center_point_def, step, radius, color):
     for _ in range(3):
-        sd.circle(center_position=center_point_def, radius=radius)
+        sd.circle(center_position=center_point_def, radius=radius, color=color, width=0)
         radius -= step
 
 
@@ -36,16 +37,23 @@ def bubble(center_point_def, step, radius):
 
 # Нарисовать три ряда по 10 пузырьков
 
-for _ in range(3):
-    y = 0
-    y += 100
-    x = 0
-    for i in range(10):
-        x += 100
-        center_point = sd.get_point(x, y)
-        bubble(center_point_def=center_point, step=5, radius=50)
+# for _ in range(3):
+#     y = 0
+#     y += 100
+#     x = 0
+#     for i in range(10):
+#         x += 100
+#         center_point = sd.get_point(x, y)
+#         bubble(center_point_def=center_point, step=5, radius=50)
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
-random
+for _ in range(1000):
+    point_100 = sd.random_point()
+    step_100 = random.randint(2, 8)
+    radius_100 = random.randint(2, 50)
+    _n = random.randint(0, 5)
+    color = colors[_n]
+    print(radius_100, step_100)
+    bubble(center_point_def=point_100, step=step_100, radius=radius_100, color=color)
 
 sd.pause()
