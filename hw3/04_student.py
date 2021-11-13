719873eae2068850c5c5fb74040142fc0c2b3925
Anton Chernichkin <chetoony@gmail.com>
2021-11-13 16:13:21 +0300

    Решение файла 04

--- hw3/04_student.py
+++ hw3/04_student.py
@@ -10,5 +10,19 @@
 #   Студенту надо попросить ХХХ.ХХ рублей
 
 educational_grant, expenses = 10000, 12000
+needed_money = 0
+total_needed_money = 0
+for i in range(4):
+    if i == 1:
+        print("Траты на обучение во " + str(i+1) + " год " + str(round(expenses, 2)) + " рублей")
+    else:
+        print("Траты на обучение в " + str(i+1) + " год " + str(round(expenses, 2)) + " рублей")
+    needed_money = expenses - educational_grant
+    total_needed_money += needed_money
+    expenses = expenses * 1.03
+    if i == 1:
+        print("Студенту надо попросить во " + str(i + 1) + " год обучения " + str(round(needed_money, 2)) + " рублей")
+    else:
+        print("Студенту надо попросить в " + str(i+1) + " год обучения " + str(round(needed_money, 2)) + " рублей")
 
-# TODO здесь ваш код
+print("Суммарно студенту нужно попросить " + str(round(total_needed_money, 2)) + " рублей на 4 года обучения")
