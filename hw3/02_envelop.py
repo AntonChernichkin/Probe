b198667f7151e89539f46f7de1d96b5264e3d933
Anton Chernichkin <chetoony@gmail.com>
2021-11-13 13:59:10 +0300

    Решение файла 02

--- hw3/02_envelop.py
+++ hw3/02_envelop.py
@@ -9,8 +9,8 @@
 # Результат проверки вывести на консоль (ДА/НЕТ)
 # Использовать только операторы if/elif/else, можно вложенные
 
-envelop_x, envelop_y = 10, 7
-paper_x, paper_y = 8, 9
+# envelop_x, envelop_y = 10, 7
+# paper_x, paper_y = 8, 9
 # проверить для
 # paper_x, paper_y = 9, 8
 # paper_x, paper_y = 6, 8
@@ -20,15 +20,19 @@ paper_x, paper_y = 8, 9
 # paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
-
+# if envelop_x > paper_x and envelop_y > paper_y:
+#     print("ДА")
+# elif envelop_y > paper_x and envelop_x > paper_y:
+#     print("ДА")
+# else:
+#     print("НЕТ")
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
 # могут быть в диапазоне от 1 до 1000)
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
+hole_x, hole_y = 8, 9
 # brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
@@ -49,4 +53,11 @@ paper_x, paper_y = 8, 9
 # brick_x, brick_y, brick_z = 3, 11, 6
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if (brick_x < hole_x and brick_y < hole_y) or (brick_y < hole_x and brick_x < hole_y):
+    print("ДА")
+elif (brick_x < hole_x and brick_z < hole_y) or (brick_z < hole_x and brick_x < hole_y):
+    print("ДА")
+elif (brick_y < hole_x and brick_z < hole_y) or (brick_z < hole_x and brick_y < hole_y):
+    print("ДА")
+else:
+    print("НЕТ")
